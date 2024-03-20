514abfd666a9c75f69ed8583efdd120dfa989cbf
Литвинов Андрей <alitvinov@mtsbank.ru>
2024-03-20 16:50:56 +0300

    HW6: junit

---
+++ hw1/src/test/java/ru/mtsbank/Services/CreateAnimalServiceImplTest.java
@@ -0,0 +1,23 @@
+package ru.mtsbank.Services;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CreateAnimalServiceImplTest {
+
+    @Test
+    public void createAnimalsRandom() {
+        CreateAnimalServiceImpl service = new CreateAnimalServiceImpl();
+        var hashMap = service.createAnimalsRandom(50);
+
+        int count = 0;
+
+        for (var entry : hashMap.entrySet()) {
+            count = count + entry.getValue().size();
+        }
+
+        Assert.assertEquals(50, count);
+    }
+}
