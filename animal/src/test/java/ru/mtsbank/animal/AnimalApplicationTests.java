690c98dfa7d250ea2d8504c86997cc1750cbd0a7
Литвинов Андрей <alitvinov@mtsbank.ru>
2024-04-16 12:21:09 +0300

    HW10 два теста

--- animal/src/test/java/ru/mtsbank/animal/AnimalApplicationTests.java
+++ animal/src/test/java/ru/mtsbank/animal/AnimalApplicationTests.java
@@ -1,7 +1,13 @@
 package ru.mtsbank.animal;
 
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.mtsbank.animal.animals.Wolf;
+import ru.mtsbank.animal.services.CreateAnimalServiceImpl;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class AnimalApplicationTests {
@@ -10,4 +16,24 @@ class AnimalApplicationTests {
 	void contextLoads() {
 	}
 
+	@Autowired
+	private Wolf wolf;
+
+	@Autowired
+	private CreateAnimalServiceImpl createImpl;
+	@Test
+	void wolfCreated() {
+		assertThat(wolf).isNotNull();
+	}
+
+	@Test
+	void createImplTest() {
+		var hashMap = createImpl.createAnimalsRandom(50);
+
+		int count = 0;
+		for (var entry : hashMap.entrySet()) {
+			count = count + entry.getValue().size();
+		}
+		assertThat(count).isEqualTo(50);
+	}
 }
